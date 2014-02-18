2e282a2c6a853effc92d6b84a1f10e8653b05757
permjen <mikeavalardo1@hotmail.com>
2014-02-18 23:04:56 +0800

    added Login page

--- app/views/frontend/user_login/index.blade.php
+++ app/views/frontend/user_login/index.blade.php
@@ -13,7 +13,52 @@
             </div>
 
   <div class="col-lg-8 col-md-8" style="margin-top:20px">
-        
+                
+
+  <div class="container">
+    <div class="row">
+        <div style="text-align: center" class="col-md-6 col-md-offset-3">
+            <h1 style="color: #FFFFFF">Alumni Tracking</h1>
+
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h5 class="text-center"><b>Log In</b></h5>
+                    {{ Form::open(array('class' => 'form-signup', 'id' => 'form-signin')) }}
+
+                    @if ($errors->has('login'))
+                    <div class="alert alert-danger">
+                        <a class="close" data-dismiss="alert" href="#" aria-hidden="true">&times;</a>{{ $errors->first('login', ':message') }}
+                    </div>
+                    @endif
+
+                    <div class="form-group">
+                        <div class="input-group">
+                            <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
+                            </span>
+                            {{ Form::text('email', null,array('class' => 'form-control', 'placeholder'=>'Email', 'autofocus'=>'')) }}
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="input-group">
+                            <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+                            {{ Form::password('password', array('class' => 'form-control','placeholder'=>'Password')) }}
+                        </div>
+                    </div>
+
+                    <label style="text-align: left" class="checkbox">{{ Form::checkbox('rememberMe', 'rememberMe') }} Remember me</label>
+
+                    {{ Form::submit('Sign in', array('class' => 'btn btn-sm btn-primary btn-block', 'role'=>'button')) }}
+                    {{ Form::close() }}
+                    <br>
+                    {{ HTML::link('/admin/forgot-password','Forgot Password', array('class' => 'btn btn-sm btn-default btn-block')) }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
   </div>
             <div class="col-lg-2 col-md-2">
                 <h3><i class="icon-folder-open-alt"></i> Blissful Layout Options</h3>
